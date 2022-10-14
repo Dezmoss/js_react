2c57d21f62cd5940e260893cd83cd148359cd8ec
Denis Kuznetsov <detrevile3@gmail.com>
2022-10-14 19:28:22 +0300

    initial commit

---
+++ part2pr3/script.js
@@ -0,0 +1,46 @@
+/* Задание на урок:
+
+1) Первую часть задания повторить по уроку
+
+2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+false - выводит в консоль главный объект программы
+
+3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
+"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
+genres
+
+P.S. Функции вызывать не обязательно*/
+
+'use strict';
+
+// Код возьмите из предыдущего домашнего задания
+
+const userInput = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const personalMovieDB = {
+    count: userInput,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: true
+};
+
+for (let i = 0; i < 2; i++) {
+    let a = prompt('Один из последних просмотренных фильмов?', '');
+    let b = prompt('На сколько оцените его?', '');
+    if (a === '' || a.length > 50 || a === null || b === '' || b === null) {
+        i--;
+        continue;
+    } else {
+        personalMovieDB.movies[a] = b;
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+} else {
+    console.log('Произошла ошибка');
+}
