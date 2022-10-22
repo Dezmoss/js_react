73d977245628a32074f77b55f7723e9f18cb3e22
Denis Kuznetsov <detrevile3@gmail.com>
2022-10-22 10:23:20 +0300

    complete

--- part2ex18/script.js
+++ part2ex18/script.js
@@ -1,2 +1,22 @@
 'use strict';
 
+function isPangram(string) {
+    let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+        'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
+        'x', 'y', 'z'];
+
+    for (let letter of string) {
+        if (alphabet.indexOf(letter.toLowerCase()) != -1) {
+            alphabet.splice(alphabet.indexOf(letter.toLowerCase()), 1);
+        }
+    }
+
+    if (alphabet.length === 0) {
+        return true;
+    } else {
+        return false;
+    }
+
+}
+
+console.log(isPangram('he quick brown fox jumps over he lazy dog'));
